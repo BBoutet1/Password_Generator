8c32d1200c72ba947de4c3472b66d8e1905cfc56
Brice Boutet <boutetlb@gmail.com>
2020-02-13 23:28:48 -0500

    Password should contain at least 3 types of characters or try again

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+   passwordText.value = password;
 
 }
 
-// This function generate a secured password followinf characters criteria
+// This function generate a secured password following characters criteria
 function generatePassword(){
 
     // password length selected by the user from 6 to 26 characters
@@ -31,23 +28,57 @@ var i;
 for (i=0; i<26; i++){ // filling alphaLower and lphaUpper using unicode
  var j=i+97;
  var k=i+65;
-alphaLower.push(String.fromCharCode(j)); //
+alphaLower.push(String.fromCharCode(j));
 alphaUpper.push(String.fromCharCode(k));
+}
+// Merging user selected characters type arrays in one big array
+alert("Select at least 3 types of caracters");
 
-// Merging all the 4 arrays in one big array
-var totalChar = alphaLower.concat(alphaUpper, number,specialChar);
+var totalChar = []; // merger array
+
+var lowerChar = confirm("Lower case characters: "+alphaLower);
+var upperChar = confirm("Upper case characters: "+alphaUpper);
+var numChar = confirm("Digital characters: "+number); 
+var speChar = confirm("Special characters: "+specialChar); 
 
 
-// randomly generated password
+var numOftypes=0; // Number of selected type of caracter
 
+if (lowerChar===true) { // lower case characters added in the merger array Char if selected
+  totalChar = totalChar.concat(alphaLower);
+  numOftypes++
+}
+if (upperChar===true) { // upper case characters added in the merger array if selected
+  totalChar = totalChar.concat(alphaUpper);
+  numOftypes++
+}
+if (numChar===true) { // digital characters added in the merger array if selected
+  totalChar = totalChar.concat(number);
+  numOftypes++
+}
+
+if (speChar===true) {totalChar =totalChar.concat(specialChar); // special characters added in the merger array if selected
+  numOftypes++
+}
+
+//Only a code with at least 3 types of characters is allowed
+
+if (numOftypes<3){ // if less than 3 types of characters have been selected
+  alert("Your password must contains at least 3  types of characters. Please try again!")
+}
+else { // at least 3 types of characters selected
+  
+// randomly generating password
 var securedPassword = "";
 for (j=0; j<numberOfChar; j++){
+ 
   var random = Math.floor(Math.random() * totalChar.length);
   securedPassword = securedPassword + totalChar[random];}
 }
 return securedPassword;
 }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click",function(){
