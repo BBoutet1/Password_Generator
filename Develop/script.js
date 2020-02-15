af1506a683b627d253f0c6c2dcc72818789d3743
Brice Boutet <boutetlb@gmail.com>
2020-02-14 23:30:39 -0500

    Password text reset if null password length or criteria not met

--- Develop/script.js
+++ Develop/script.js
@@ -12,30 +12,35 @@ function writePassword() {
 // This function generate a secured password following characters criteria
 function generatePassword(){
 
-    // password length selected by the user from 6 to 26 characters
+// Password length should be between 8 and 26 characters
 var numberOfChar = prompt("Please, enter the desired number of characters");
-if (numberOfChar<8 || numberOfChar>28){ 
-alert("Your password should contains 8 to 26 characters. Please try again!")
-return passwordText;
+if( numberOfChar==null) { // if no value is entered
+  alert("Number of characters: no input value")
+  passwordText = document.querySelector("#password").innerHTML;// Password text reset
+  return passwordText;
+  console.log(numberOfChar);
 }
-else {
-  // 1 array for each of the 4 types of character
+else if ((numberOfChar<8 || numberOfChar>28) && !(numberOfChar==null)){ // password length out of [8,26]  range 
+alert("Your password should contains 8 to 26 characters. Please try again!")
+passwordText = document.querySelector("#password").innerHTML;// Password text reset in display field
+return passwordText;
+} 
+else { //Password length in [8,26] range 
 var alphaLower = []; // Lower case array
 var alphaUpper = []; // Upper case array
-var number = [0,1,2,3,4,5,6,7,8,9]; // digit array
-var specialChar = ("!#$%&'()*+-/<=>?@[\]^_`{|}~").split(""); // Special character array
+var number = [0,1,2,3,4,5,6,7,8,9]; // digital characters array
+var specialChar = ("!#$%&'()*+-/<=>?@[\]^_`{|}~").split(""); // Special characters array
 var i;
-for (i=0; i<26; i++){ // filling alphaLower and lphaUpper using unicode
+for (i=0; i<26; i++){ // filling alphaLower and lphaUpper with alphabet using unicode
  var j=i+97;
  var k=i+65;
 alphaLower.push(String.fromCharCode(j));
 alphaUpper.push(String.fromCharCode(k));
 }
-// Merging user selected characters type arrays in one big array
+
+// Merging user selected array in one big array
 alert("Select at least 3 types of caracters");
-
 var totalChar = []; // merger array
-
 var lowerChar = confirm("Lower case characters: "+alphaLower);
 var upperChar = confirm("Upper case characters: "+alphaUpper);
 var numChar = confirm("Digital characters: "+number); 
@@ -61,13 +66,15 @@ if (speChar===true) {totalChar =totalChar.concat(specialChar); // special charac
   numOftypes++
 }
 
-//Only a code with at least 3 types of characters is allowed
 
+//User should chose 3 types of characters at least to generate a secure password
 if (numOftypes<3){ // if less than 3 types of characters have been selected
   alert("Your password must contains at least 3  types of characters. Please try again!")
+  passwordText = document.querySelector("#password").innerHTML;// Password text reset in display field
+  return passwordText;
 }
 else { // at least 3 types of characters selected
-  
+
 // randomly generating password
 var securedPassword = "";
 for (j=0; j<numberOfChar; j++){
@@ -82,13 +89,7 @@ return securedPassword;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click",function(){
-
-
-
-
   writePassword()
-
-
 });
 
 
